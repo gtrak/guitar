20946ac290f3bb429e2e4addcf9f75fd8da1b923
Gary Trakhman <gary@arena.io>
2018-09-22 12:41:09 -0400

    Support more generators

--- src/guitar/core.clj
+++ src/guitar/core.clj
@@ -55,23 +55,7 @@
   (nth (from string)
        fret))
 
-(notes-of-fret :E 1)
-
-(defn random-notes
-  []
-  (->> all-the-notes shuffle cycle))
-
-(defn random-string-notes
-  [string]
-  (map (fn [note]
-         (let [note (rand-nth note)]
-           [note (frets-of-note string note)]))
-       (random-notes)))
-
-(take 5 )
-;; (= (fret :E :A) [5 17])
-
-(first (random-string-notes :E))
+;; (notes-of-fret :E 1)
 
 (defn prompt [string note]
   (println "String: " (name string)
@@ -79,20 +63,64 @@
            " Fret?")
   (clojure.string/trim (read-line)))
 
+(defrecord GuitarNote [string note frets])
+
+(defn random-notes
+  []
+  (->> all-the-notes
+       shuffle
+       cycle))
+
+(defn string-note
+  [string note]
+  (GuitarNote. string
+               note
+               (frets-of-note string note)))
+
+(defn random-string-notes
+  [string]
+  (map (fn [note-names]
+         (string-note string (rand-nth note-names)))
+       (random-notes)))
+
+;; (first (random-string-notes :E))
+
+(defn new-string
+  []
+  (let [string (rand-nth strings)]
+    (random-string-notes string)))
+
+(defn one-string
+  [string]
+  (random-string-notes string))
+
+(defn between-frets
+  [a b]
+  (let [[min max] [(min a b) (max a b)]]
+    (mapcat
+     (fn [note-names]
+       (for [string strings
+             :let [note (string-note string (rand-nth note-names))]
+             :when (some (fn [fret]
+                           (<= min fret max))
+                         (:frets note))]
+         note))
+     (random-notes))))
+
+;; (take 5 (between-frets 8 12))
+
 (defn game
   []
-  (let [new-string (fn []
-                     (let [string (rand-nth strings)]
-                       [string (random-string-notes string)]))]
-    (loop [[string notes] (new-string)]
-      (let [[[note frets] & more] notes
+  (let [gen (partial between-frets 5 9)]
+    (loop [notes (gen)]
+      (let [[{:keys [string note frets]} & more] notes
             frets (set frets)
             input (prompt string note)]
         (when (seq input)
           (if (frets (read-string input))
             (println "Correct!")
             (println "Incorrect! It was " frets))
-          (recur (new-string)))))))
+          (recur more))))))
 
 ;; (game)
 
