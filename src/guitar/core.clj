e4157d48199e6a2f7e7295e27562bfa9f0fed7d9
Gary Trakhman <gary@arena.io>
2018-09-23 19:21:58 -0400

    note-at-fret

--- src/guitar/core.clj
+++ src/guitar/core.clj
@@ -14,6 +14,42 @@
    [:G]
    [:G# :Ab]])
 
+(def same-notes
+  "Normalize sharps to flats"
+  {:A :A
+   :A# :Bb
+   :Bb :Bb
+   :B :B
+   :C :C
+   :C# :Db
+   :Db :Db
+   :D :D
+   :D# :Eb
+   :Eb :Eb
+   :E :E
+   :F :F
+   :F# :Gb
+   :Gb :Gb
+   :G :G
+   :G# :Ab
+   :Ab :Ab})
+
+(defn note-str
+  [a]
+  (clojure.string/lower-case (name a)))
+
+(def same-notes-strtable
+  (reduce-kv (fn [acc k v]
+               (conj acc [(note-str k)
+                          (note-str v)]))
+             {}
+             same-notes))
+
+(defn same-note?
+  [a b]
+  (= (same-notes-strtable (note-str a))
+     (same-notes-strtable (note-str b))))
+
 (defn containing-note
   [note within-notes]
   (some #{note} within-notes))
@@ -61,7 +97,15 @@
   (println "String: " (name string)
            " Note: " (name note)
            " Fret?")
-  (clojure.string/trim (read-line)))
+  (if-let [l (read-line)]
+    (clojure.string/trim l)))
+
+(defn prompt-fret [string fret]
+  (println "String: " (name string)
+           " Fret: " fret
+           " Note?")
+  (if-let [l (read-line)]
+    (clojure.string/trim l)))
 
 (defrecord GuitarNote [string note frets])
 
@@ -96,7 +140,8 @@
 
 (defn between-frets
   [a b]
-  (let [[min max] [(min a b) (max a b)]]
+  (let [[min max] [(min a b)
+                   (max a b)]]
     (mapcat
      (fn [note-names]
        (for [string strings
@@ -109,17 +154,25 @@
 
 ;; (take 5 (between-frets 8 12))
 
+
+
 (defn game
   []
   (let [gen (partial between-frets 5 9)]
     (loop [notes (gen)]
       (let [[{:keys [string note frets]} & more] notes
-            frets (set frets)
-            input (prompt string note)]
+            ;;frets (set frets)
+            ;;input (prompt string note)
+            input (prompt-fret string (rand-nth frets))]
         (when (seq input)
-          (if (frets (read-string input))
+          (println input)
+          ;; TODO: d#/eb are same note
+          (if (same-note? note input)
+            ;;(frets (read-string input))
             (println "Correct!")
-            (println "Incorrect! It was " frets))
+            ;;(println "Incorrect! It was " frets)
+            (println "Incorrect! It was " (name note))
+            )
           (recur more))))))
 
 ;; (game)
